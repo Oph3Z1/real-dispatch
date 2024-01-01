6ffd440c98c3816dbabe0fadcecd16bb9d5377bb
Oph3Z <59979752+Oph3Z1@users.noreply.github.com>
2024-01-01 01:03:54 +0100

    0.0.1

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+Config.Framework = 'autodetect' -- newesx, oldesx, newqb, oldqb, autodetect
+Config.MySQL = 'oxmysql' -- oxmysql, ghamattimysql, mysql-async | Don't forget to edit fxmanifest.lua
--- html/app.js
+++ html/app.js
@@ -7,11 +7,93 @@ const store = Vuex.createStore({
 const app = Vue.createApp({
     data: () => ({
         Show: true,
+        Dispatches: {},
+        MAP_LIST: {},
     }),
 
     methods: {
+        CreateMAP(key, id) {
+            const CUSTOM_CRS = L.extend({}, L.CRS.Simple, {
+                projection: L.Projection.LonLat,
+                scale: function(zoom) { 
+                    return Math.pow(1, zoom - 4);
+                },
+                zoom: function(sc) {
+                    return Math.log(sc) / 1;
+                },
+                distance: function(pos1, pos2) {
+                    const x_difference = pos2.lng - pos1.lng;
+                    const y_difference = pos2.lat - pos1.lat;
+                    return Math.sqrt(x_difference * x_difference + y_difference * y_difference);
+                },
+                transformation: new L.Transformation(0.02072, 117.3, -0.0205, 172.8),
+                infinite: false
+            });
+            
+            this.MAP_LIST[key] = L.map(id, {
+                crs: CUSTOM_CRS,
+                
+            
+                noWrap: true,
+                continuousWorld: false,
+                preferCanvas: true,
+            
+                center: [0, -1024],
+                zoom: 4,
+            });  
+            
+            const MAP = this.MAP_LIST[key];
 
-    },  
+            const MAP_IMAGE = 'https://gta-assets.nopixel.net/images/dispatch-map.png';
+            const SW = MAP.unproject([0, 1024], 3 - 1);
+            const NE = MAP.unproject([1024, 0], 3 - 1);
+            const MAP_BOUNDS = new L.LatLngBounds(SW, NE);
+            MAP.setMaxBounds(MAP_BOUNDS);            
+
+            MAP.attributionControl.setPrefix(false)
+
+            L.imageOverlay(MAP_IMAGE, MAP_BOUNDS).addTo(MAP);
+
+            MAP.on('drag', function() {
+                MAP.panInsideBounds(MAP_BOUNDS, { animate: false });
+            });
+        },
+
+        // DispatchMAP(DISPATCH) {
+        //     const MIN = Math.round(Math.round((new Date() - new Date(DISPATCH.time)) / 1000) / 60);
+        //     if (MIN > 10)
+        //         return;
+
+        //     Object.keys(this.MAP_LIST).forEach(key => {
+        //         const value = this.MAP_LIST[key];
+
+        //         const COORDS_X = DISPATCH.origin.x;
+        //         const COORDS_Y = DISPATCH.origin.y;
+        //         const CODE = DISPATCH.callId;
+
+        //         const ICON_TYPE = this.DispatchPing;
+
+        //         this.Dispatches[CODE] = L.marker([COORDS_Y, COORDS_X], { icon: ICON_TYPE });
+
+        //         this.Dispatches[CODE].bindTooltip(`<div class="map-tooltip-info">${DISPATCH.dispatchMessage}</div><div class="map-tooltip-resp"><b>${Object.keys(DISPATCH.units).length}</b> units responding.</div><div class="map-tooltip-id">#${DISPATCH.callId}</div>`, {
+        //             direction: 'top',
+        //             permanent: false,
+        //             offset: [0, -10],
+        //             opacity: 1,
+        //             interactive: true,
+        //             className: 'map-tooltip'
+        //         });
+
+        //         this.Dispatches[CODE].addTo(value);
+        //     });
+        // },
+
+        // ClearMap() {
+        //     $(".leaflet-popup-pane").empty();
+        //     $(".leaflet-marker-pane").empty();
+        // }   
+    },
+
     computed: {
         
     },
@@ -22,8 +104,17 @@ const app = Vue.createApp({
 
     beforeDestroy() {
         window.removeEventListener('keyup', this.onKeyUp);
-     },
+    },
+
     mounted() {
+        this.CreateMAP("small", this.$refs.smallMap);
+
+        setTimeout(() => {
+            const map = this.MAP_LIST["small"];
+            const bounds = map.getBounds();
+            map.fitBounds(bounds, { animate: false });        
+        }, 100);        
+        
         window.addEventListener("message", event => {
             window.addEventListener('keyup', this.onKeyUp);
             switch (event.data.message) {
@@ -36,7 +127,6 @@ const app = Vue.createApp({
                 break;
             }   
         });
-        
     },
     
 });
